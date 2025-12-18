86ee0302bc265d3b026515c4d6e17531676aa3c2
galina <lukinskaya.alina@gmail.com>
2025-12-17 21:52:40 -0500

    Update readme, add small fix

--- organizer.py
+++ organizer.py
@@ -64,7 +64,7 @@ def organize_files(current_dir_path):
 
         if os.path.isfile(entry_path):  # Files
             file_entry = FileEntry(entry_path)
-            FileEntry.categorize(file_entry)
+            file_entry.categorize()
 
             if file_entry.category == FileCategory.PROJECT:
                 stats.add_skipped()
